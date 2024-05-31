8fa8f5ae6d35ffe631227b3b9aaab461edd8ac9c
Julian Ascanio <julianandresac@ufps.edu.co>
2024-05-31 10:30:42 -0500

    Primer commit

---
+++ src/main/java/com/mangas/SegundoParcialDeProgramacionWebApplication.java
@@ -0,0 +1,13 @@
+package com.mangas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegundoParcialDeProgramacionWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegundoParcialDeProgramacionWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mangas/SegundoParcialDeProgramacionWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mangas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegundoParcialDeProgramacionWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
